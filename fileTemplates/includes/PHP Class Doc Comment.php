3a18695bde00f523d80fa86e41c298877951cf49
Andew Dallow <andrew.dallow@ecommistry.com>
2018-05-16 17:12:36 +1200

    PS-2018.1.3 <Andew Dallow@DESKTOP-4CII2S6 Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, composerJson.xml, project.default.xml, laf.xml, ea_extended.xml, KeyPromoterXStatistic.xml, PHP Class Doc Comment.php, XML Mage Extention Registration.xml, Default.xml, PHP File Header.php, ui.lnf.xml, log_highlighting.xml, material_theme.xml, colors.scheme.xml, databaseSettings.xml, notifications.xml, _@user_Darcula.icls, databaseDrivers.xml, debugger.xml

---
+++ fileTemplates/includes/PHP Class Doc Comment.php
@@ -0,0 +1,13 @@
+/**
+* Short description for class
+*
+* Long description for class (if any)...
+*
+* @category   Zend
+* @package    Zend_
+* @subpackage 
+* @copyright  Copyright (c) 2018 ecommistry (http://www.ecommistry.com)
+* @license    http://framework.zend.com/license   BSD License
+* @version    Release: @package_version@
+* @link       http://framework.zend.com/package/PackageName
+*/
